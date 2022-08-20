10d7971e6af8b4563de9a36056b1b73edc285209
orcephrye <drakethebanditi@yahoo.com>
2022-08-20 14:24:00 -0500

    First Commit

---
+++ PyMidiController/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+
+# Author: Ryan Henrichson
+# Version: 0.1
+# Date: 08/20/2022
+# Description:
+
+
+import sys
+
+
+def main():
+    print('Starting app')
+
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
